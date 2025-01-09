c79f91f9df24fe85088fa39bf6238c891ff299b8
Admin <Admin@F-4>
2025-01-09 16:53:37 +0900

    day2

---
+++ Project1/변수.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//한줄주석
+	/*
+	여
+	러
+	줄
+	주
+	석
+	 */
+	//int num = 3;
+	//자료형 변수명 = 값;
+	//자료형 변수명2; // 선언
+	//변수명2 = 값; // 초기화
+	// //변수명3= 값; 안됨
+
+	int num2; //선언
+	num2 = 2;//초기화
+	num2 = 5;//초기화 
+
+	int number1, number2;
+	number1 = 10; //number1 초기화
+	number2 = 20; //number2 초기화
+
+	int number3 = 30, number4 = 40;
+	
+	//변수명 규칙 
+	int num = 50;
+	int number16 = 16;//숫자조합 가능
+	int number_20 = 20;//언더바 가능
+	int Number = 165;//대문자 가능
+
+	//불가능한 변수명
+	//int hello World = 1;//공백불가
+	//int hell@% $% = 2;//특수문자불가
+	//int 12number = 12;//숫자먼저 사용불가
+	//int int = 1;//예약어 불가
+
+	//1.number_five
+	//2.numberFive
+
+	return 0;
+}
